0eeb093efee9784ae06650ef6abe2d541739d50f
Lukas Frey <mail@lukasfrey.cz>
2025-07-23 16:07:46 +0200

    feat: don't rerender calendar on wire update

--- resources/views/livewire/reservations/create-reservation.blade.php
+++ resources/views/livewire/reservations/create-reservation.blade.php
@@ -1,8 +1,10 @@
 <div class="flex flex-col gap-6">
-    <x-auth-header :title="__('Make a reservation')" :description="__('Please fill out the form below to reserve a table')" />
+    <x-auth-header :title="__('Make a reservation')"
+                   :description="__('Please fill out the form below to reserve a table')"
+    />
 
     <!-- Session Status -->
-{{--    <x-auth-session-status class="text-center" :status="session('status')" />--}}
+    {{--    <x-auth-session-status class="text-center" :status="session('status')" />--}}
 
     <form wire:submit="createReservation" class="flex flex-col gap-6">
         <!-- Guests count -->
@@ -22,7 +24,8 @@
                 maxDate: @js(today()->addDays(config('app.reservation_max_days'))),
                 disabledDates: @js($this->getDisabledDates())
             })"
-            class="w-full"
+                 class="w-full"
+                 wire:ignore
             ></div>
             <flux:error name="date"></flux:error>
         </flux:field>
@@ -47,5 +50,4 @@
             <flux:button variant="primary" type="submit" class="w-full">{{ __('Reserve table now') }}</flux:button>
         </div>
     </form>
-    {{-- Success is as dangerous as failure. --}}
 </div>
