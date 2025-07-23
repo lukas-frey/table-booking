df525c40d8b5d8958fa09c5be320798300d18d4d
Lukas Frey <mail@lukasfrey.cz>
2025-07-23 14:11:05 +0200

    feat: add reservation model casts

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -18,6 +18,14 @@ class Reservation extends Model
         'user_id',
     ];
 
+    protected function casts(): array
+    {
+        return [
+            'starts_at' => 'datetime',
+            'ends_at' => 'datetime',
+        ];
+    }
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
