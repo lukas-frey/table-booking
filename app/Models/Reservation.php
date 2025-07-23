7943bfc5d4a7294bedbf3ba898ae3de213b5235c
Lukas Frey <mail@lukasfrey.cz>
2025-07-23 12:13:57 +0200

    feat: add reservation model

---
+++ app/Models/Reservation.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reservation extends Model
+{
+    /** @use HasFactory<\Database\Factories\ReservationFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'guests',
+        'starts_at',
+        'ends_at',
+        'user_id',
+    ];
+}
---
+++ database/factories/ReservationFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Reservation>
+ */
+class ReservationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $startsAt = $this->faker->dateTimeBetween('-14 days', '+ 14 days');
+        $endsAt = Carbon::parse($startsAt)->addHours(config('app.reservation_duration'));
+
+        return [
+            'guests' => $this->faker->numberBetween(1, config('app.seats_per_table')),
+            'starts_at' => $startsAt,
+            'ends_at' => $endsAt,
+            'user_id' => User::factory(),
+        ];
+    }
+}
