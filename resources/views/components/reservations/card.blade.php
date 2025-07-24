ea0d56168233529821004428eec00fe3bc296aa4
Lukas Frey <mail@lukasfrey.cz>
2025-07-24 13:52:43 +0200

    feat: add ongoing reservation badge and remove cancel button when ongoing

--- resources/views/components/reservations/card.blade.php
+++ resources/views/components/reservations/card.blade.php
@@ -4,10 +4,14 @@
 
 <flux:callout class="w-full mb-4 group-last:last:mb-0">
     <div class="flex flex-col gap-2">
-        <flux:heading>
-            {{$reservation->starts_at->format('j. n. Y')}}
-        </flux:heading>
 
+        <flux:heading class="w-full flex items-center justify-between gap-2">
+            <span>{{$reservation->starts_at->format('j. n. Y')}}</span>
+
+            @if(now()->isBetween($reservation->starts_at, $reservation->ends_at))
+                <flux:badge size="sm" color="lime">{{__('Ongoing')}}</flux:badge>
+            @endif
+        </flux:heading>
         <div class="flex flex-row flex-wrap gap-4">
             <span class="text-zinc-500 dark:text-zinc-400 inline-flex items-center gap-1">
                 <flux:icon name="clock" class="size-4"/>
@@ -20,8 +24,10 @@
             </span>
         </div>
 
-        <flux:button href="#" size="sm" icon="x-circle" class="text-sm mr-auto">
-            {{__('Cancel reservation')}}
-        </flux:button>
+        @if(! now()->isBetween($reservation->starts_at, $reservation->ends_at))
+            <flux:button href="#" size="sm" icon="x-circle" class="text-sm mr-auto">
+                {{__('Cancel reservation')}}
+            </flux:button>
+        @endif
     </div>
 </flux:callout>
